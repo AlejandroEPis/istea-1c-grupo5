5f4ef7d12e7a660ead264a35cd385339ce2f01ea
Lucas <llgondariz@gmail.com>
2026-04-19 02:55:00 -0300

    Nuevo box de relojes destacados, con personalizacion diferente, se edita html, css y js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,41 @@ console.log(getProducts());
 //console.log(productList);
 
 graficarTarjetas('#showcaseprincipal',5);
+graficarTarjetas('#showcaseremix',3);
+graficarDestacados();
 
 
 
 
+function graficarDestacados(){
+let destacado1 = document.querySelector(`#highlight-item-1`);
+let destacado2 = document.querySelector(`#highlight-item-2`);
+let destacado3 = document.querySelector(`#highlight-item-3`);
+let destacados = [destacado1,destacado2,destacado3];
+let template = '';
+getProducts().then((products) => {
+    products.slice(0,3).forEach((p, index) => {
+        template = `
+        <div class="highlight-card-image">
+        <img src="${p.thumbnail}" class="card-img-top" alt="${p.title}">
+        </div>
+        <h3>${p.title}</h3>
+        <p>
+            ${p.description}
+        </p>
+`;
+        destacados[index].innerHTML = template;
+    });
+});
 
 
+}
 
 function graficarTarjetas(id,cantidad){
 let cardList = document.querySelector(`${id}`);
 getProducts().then((products) => {
     let template = '';
-    products.slice(0,5).forEach((p) => { // Corto el array para solo mostrar los primeros 5
+    products.slice(0,cantidad).forEach((p) => { // Corto el array para solo mostrar los primeros 5
         template += `
     <div class="card">
         <img src="${p.thumbnail}" class="card-img-top" alt="${p.title}">
