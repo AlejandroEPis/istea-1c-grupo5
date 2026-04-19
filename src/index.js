dd7289f91d04a7bd5a7d4855d37990e5ab0cf8f4
Lucas <llgondariz@gmail.com>
2026-04-19 18:22:44 -0300

    Agrego mas filas a la pagina principal, edito html

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { graficarTarjetas } from './render.js';
 graficarTarjetas('#showcaseprincipal',5);
 graficarTarjetas('#showcaseremix',3);
 graficarDestacados();
+graficarTarjetas('#showcaseMechanical',5);
+graficarTarjetas('#showcaseGilded',4);
+
 
 
 
